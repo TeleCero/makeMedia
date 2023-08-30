e3adebce70af46d681935d9aae4e1174b74e5261
naielv <109038805+naielv@users.noreply.github.com>
2023-08-30 22:51:40 +0200

    v2

--- README.md
+++ README.md
@@ -3,12 +3,16 @@ YT-DLP Downloader for TeleCero
 
 Over 100 Links in TeleCero
 
+## Warning
+Please add your python bin folder to your $PATH for this to work.
+
 ## Playlist 2.0 Format
 ```
 Channel Name|Channel URL
 ```
 ## Examples
 ### Mitele
+Included by default as ```salvame_2023```.
 ```
 Salvame|Video 1 URL
 Salvame|Video 2 URL
--- download.py
+++ download.py
@@ -1,4 +1,5 @@
-import os 
+import os
+import requests
 from time import sleep
 DEFAULT_ARGS = "-f 'best[height<=480][ext=mp4]/bestvideo[height<=480][ext=mp4]+bestaudio[ext=m4a]/best[height<=480]'"
 DEFAULT_PATH = "/srv/mergerfs/R01/Datos/TeleCero/Videos"
@@ -8,17 +9,14 @@ def dfile(file: str, args: str = DEFAULT_ARGS, out: str = DEFAULT_PATH):
       line = line.split("|")
       os.system(f"mkdir -p '{out}/{line[0]}'")
       os.system(f"yt-dlp -P '{out}/{line[0]}' {args} '{line[1]}'")
-def dyear(year, out: str = DEFAULT_PATH):
-  path = out + "/" + str(year)
-  dfile(f"./playlist/{str(year)}.txt")
-
-def parseyear(inp: str):
-  return inp.split(":", 1)
+def dmode(mode, out: str = DEFAULT_PATH):
+  path = out + "/" + str(mode)
+  dfile(f"./playlist/{str(mode)}.txt")
 
 if __name__ == "__main__":
   print("The default path is:")
   print(DEFAULT_PATH)
   path = input("Corrected Path Without Trailing Slash\n> ")
-  years = input("What years do you want to download?\nSeparate with comma\n> ").split(":", 1)
-  for year in years:
-    dyear(year)
+  modes = input("What modes do you want to download?\nSeparate with comma\n> ").split(":", 1)
+  for mode in modes:
+    dmode(mode)
